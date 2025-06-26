de3d4eb1b94b1936cd3cf1fb93881f780c28d937
hrisi14 <hristinafrangova.hf@gmail.com>
2025-06-26 22:13:17 +0300

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Cube3D />
+</template>
+
+<script>
+import Cube3D from './components/Cube3D.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Cube3D
+  }
+};
+</script>
+
+<style>
+body, html, #app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
